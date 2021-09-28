7bff58f680da6b90d4a22c8d05608c4ffcedd946
Knut Sorknes <knut.sorknes@makingwaves.com>
2021-09-28 13:52:58 +0200

    feat: convert css to scss + add google fonts + expand tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,31 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
     mode: "jit",
     purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
     // more options here
+    theme: {
+        fontFamily: {
+            "pt-sans": ['"PT Sans"', "sans-serif"],
+            "playfair-display": ['"Playfair Display"', "serif"],
+        },
+        colors: {
+            transparent: "transparent",
+            black: colors.black,
+            white: colors.white,
+            coolGray: colors.coolGray,
+            orange: {
+                50: "#fbece8",
+                100: "#f4c5ba",
+                200: "#ec9e8d",
+                300: "#e4775f",
+                400: "#dd5031",
+                500: "#d93d1a",
+                600: "#ae3115",
+                700: "#822510",
+                800: "#57180a",
+                900: "#2b0c05",
+            },
+        },
+    },
 };
