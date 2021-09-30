1483633d5467d283136c1c974a2cf78dc338fb0e
Knut Sorknes <knut.sorknes@makingwaves.com>
2021-09-30 15:55:46 +0200

    feat: Add responsive padding

--- src/components/SiteHeader/SiteHeader.tsx
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -9,10 +9,12 @@ import Nav from "../Nav/Nav";
 const SiteHeader = () => {
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
-    const componentClass = cx(`w-full p-10 flex flex-1 justify-between border-b border-coolGray-200 fixed z-40`);
-
     return (
-        <header className={componentClass}>
+        <header
+            className={cx(
+                `w-full p-4 md:p-8 lg:p-10 flex flex-1 justify-between border-b border-coolGray-200 fixed z-40`
+            )}
+        >
             <Logo />
 
             <Hamburger isActive={isMenuOpen} onClick={() => setIsMenuOpen(!isMenuOpen)} />
