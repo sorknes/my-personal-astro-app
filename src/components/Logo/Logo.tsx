7d09feac9598e3697d0a48ac7c79ed849915b9bb
Knut Sorknes <knut.sorknes@makingwaves.com>
2021-10-01 11:59:29 +0200

    feat: Add hover, focus and active style

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import cx from "classnames";
 
+// Utils
+import { prefix } from "../../utils/namespace";
+
+import "./logo.scss";
+
 const Logo = () => {
     return (
-        <a href="/" className={cx(`text-coolGray-900 border-0 hover:text-coolGray-900`)}>
+        <a href="/" className={cx(`${prefix}-logo`)}>
             <h1 className={`text-base font-bold`}>Knut Sorknes</h1>
         </a>
     );
