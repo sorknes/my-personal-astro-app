00655d1dd75da3d4464275705d3e94c3bd81ee29
Knut Sorknes <knut.sorknes@makingwaves.com>
2021-11-02 16:06:04 +0100

    chore: Configure and extend Tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,15 @@
 module.exports = {
     mode: "jit",
     purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
+    theme: {
+        container: {
+            center: true,
+            padding: "1rem",
+        },
+        extend: {
+            zIndex: {
+                "-1": "-1",
+            },
+        },
+    },
 };
