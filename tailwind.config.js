ced980bdf6ff9847e6d0ba9312b689625c12c45e
Knut Sorknes <knut.sorknes@makingwaves.com>
2021-11-04 10:22:44 +0100

    feat: Add google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
     mode: "jit",
     purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
     theme: {
+        fontFamily: {
+            "pt-sans": ['"PT Sans"', "sans-serif"],
+            "playfair-display": ['"Playfair Display"', "serif"],
+        },
         container: {
             center: true,
             padding: "1rem",
