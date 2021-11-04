555307bbc8bb88593151aafdae33a9a6ef5db244
Knut Sorknes <knut.sorknes@makingwaves.com>
2021-11-04 10:23:03 +0100

    feat: Grid lines + change color scheme

--- src/components/BaseGrid/index.tsx
+++ src/components/BaseGrid/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const BaseGrid = () => {
     return (
-        <div className="container fixed top-0 h-screen -translate-x-1/2 border-l border-r border-gray-200 pointer-events-none left-1/2 -z-1">
-            <span className="absolute block w-px h-screen bg-gray-200 left-4" />
-            <span className="absolute block w-px h-screen bg-gray-200 left-1/2 -translate-x-2/4" />
-            <span className="absolute block w-px h-screen bg-gray-200 right-4" />
+        <div className="container fixed top-0 h-screen -translate-x-1/2 border-l border-r border-gray-900 pointer-events-none left-1/2 -z-1">
+            <span className="absolute block w-px h-screen bg-gray-900 left-3.5" />
+            <span className="absolute block w-px h-screen bg-gray-900 left-1/2 -translate-x-2/4" />
+            <span className="absolute block w-px h-screen bg-gray-900 right-3.5" />
         </div>
     );
 };
--- src/components/LayoutHeader/index.tsx
+++ src/components/LayoutHeader/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+// Utils
+import { prefix } from "../../config/namespace";
+
+import "./style.scss";
+
 const LayoutHeader = () => {
     return (
-        <header className="container fixed flex items-center justify-between py-10 bg-white left-1/2 -translate-x-2/4">
-            <div>logo</div>
+        <header
+            className={`container fixed flex items-center justify-between py-10 bg-gray-900 left-1/2 -translate-x-2/4 z-50 text-white ${prefix}-header`}
+        >
+            <div>Knut Sorknes</div>
             <nav>
                 <button className="flex flex-col">
                     <span>-</span>
